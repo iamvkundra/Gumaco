2907c4884fdb6afe333be52aa257dcb8d535f348
iamvkundra <mkmayank39@gmail.com>
2020-07-23 18:06:16 +0530

    removed Comments

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = new express.Router()
 const auth = require('../JWTauth/auth')
 const uC = require('../controller/userController')
-// REGISTER THE USER AND LOGIN WITH THE SAME TIME
 
 router.post('/user/register',uC.userRegister)
 
@@ -13,7 +12,7 @@ router.post('/user/logout',auth,uC.userLogout)
 router.post('/user/logoutall',auth,uC.userLogoutAll)
 
 router.get('/user/authorization/check',auth,(req,res)=>{
-   // const search = req.query.
+   
     res.send("working")
 })
 
